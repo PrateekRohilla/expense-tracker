d92089d532d3784114b92136e48ad1d5e1674a06
prateekrohilla <prateek98rohilla@gmail.com>
2020-03-30 06:47:26 +0530

    created table structure

--- script.js
+++ script.js
@@ -2,6 +2,14 @@
 // get the heading element
 const headingEl = document.querySelector('#headingTotal');
 
+// get the desc element
+const inputDescEl = document.querySelector('#inputDesc');
+
+//refernce to input amount
+const inputElement = document.querySelector('#inputAmount');
+
+//get the reference to table
+const expenseTableEl = document.querySelector('#expenseTable');
 
 // init value of expense at 0
 let totalExpense = 0;
@@ -9,22 +17,51 @@ let totalExpense = 0;
 // set the heading element to totalExpense
 headingEl.textContent = totalExpense;
 
+//allExpenses at one place
+const allExpenses = [];
+
 //onButtonClick add inputAmount to totalExpense
 
 function addExpenseToTotal() {
+
+    const expenseItem = {};
+
     //read value from inputAmount
-    const inputElement = document.querySelector('#inputAmount');
     const textAmount = inputElement.value;
 
+    //read desc from inputDesc
+    const textDesc = inputDescEl.value;
+
+    console.log({textDesc,textAmount});
 
     //convert it to number
     const expense = parseInt(textAmount, 10);
+
+    //put it in object
+    expenseItem.desc = textDesc;
+    expenseItem.amount = expense;
+
+    allExpenses.push(expenseItem);
+
+    console.clear();
+    console.table(allExpenses);
 
     //add that value to totalExpense
     totalExpense = totalExpense + expense;
 
     // set the heading element to totalExpense
-    headingEl.textContent = totalExpense;
+    const someText = `Total : ${totalExpense}`;
+    headingEl.textContent = someText;
+
+
+    const allExpenseHTML = allExpenses.map(expense => {
+        return `<div>${expense.amount} :: ${expense.desc}</div>`
+    });
+
+    const joinedAllExpenseHTML = allExpenseHTML.join("");
+
+
+    expenseTableEl.innerHTML = joinedAllExpenseHTML;
 }
 
 
@@ -33,3 +70,4 @@ const element = document.querySelector("#btnAddExpense");
 
 //listen to click event
 element.addEventListener("click", addExpenseToTotal, false);
+
