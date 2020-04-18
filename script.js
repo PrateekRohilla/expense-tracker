a83eb5a994ff9fc60634dd539e010580494d7daa
prateekrohilla <prateek98rohilla@gmail.com>
2020-04-18 08:52:48 +0530

    Fix: Delete Item

--- script.js
+++ script.js
@@ -1,60 +1,53 @@
-// get the heading element
+//get reference to the elements
 const headingEl = document.querySelector("#headingTotal");
-
-// get the desc element
 const inputDescEl = document.querySelector("#inputDesc");
-
-//refernce to input amount
 const inputElement = document.querySelector("#inputAmount");
-
-//get the reference to table
 const expenseTableEl = document.querySelector("#expenseTable");
+const element = document.querySelector("#btnAddExpense");
 
 // init value of expense at 0
 let totalExpense = 0;
-
-// set the heading element to totalExpense
 headingEl.textContent = totalExpense;
+renderTotal(totalExpense);
 
 //allExpenses at one place
-const allExpenses = [];
+let allExpenses = [];
+let newArr = [];
 
 //onButtonClick add inputAmount to totalExpense
-
 function addExpenseToTotal() {
+  
+  //obj for storing expense item
   const expenseItem = {};
 
-  //read value from inputAmount
+  //read value from inputs
   const textAmount = inputElement.value;
-
-  //read desc from inputDesc
   const textDesc = inputDescEl.value;
 
   //convert it to number
   const expense = parseInt(textAmount, 10);
+
+  //add value to totalExpense
+  totalExpense = totalExpense + expense;
+  renderTotal(totalExpense);
 
   //put it in object
   expenseItem.desc = textDesc;
   expenseItem.amount = expense;
   expenseItem.moment = new Date();
 
+  //store object in array
   allExpenses.push(expenseItem);
 
-  //add that value to totalExpense
-  totalExpense = totalExpense + expense;
+  document.querySelector('#inputAmount').value='';
+  document.querySelector('#inputDesc').value='';
 
-  // set the heading element to totalExpense
-  const someText = `Total : ${totalExpense}`;
-  headingEl.textContent = someText;
-
+  //display added items in expenseTable
   renderList(allExpenses);
 
 }
 
-//get the button element
-const element = document.querySelector("#btnAddExpense");
-
-//listen to click event
+//listen to click event on add button
 element.addEventListener("click", addExpenseToTotal, false);
 
 //controller function -- formatting of date
@@ -69,20 +62,36 @@ function getDateString(moment){
 
 }
 
-//controller function -- delete Items
+//Delete items
 function deleteItem(dateValue){
-    const newArr = [];
-
-    for (let i = 0; i < allExpenses.length; i++) {
-        if (allExpenses[i].moment.valueOf() !== dateValue) {
-            newArr.push(allExpenses[i]);
-        }
-        
+    newArr=[];
+    allExpenses=allExpenses.filter((expense)=>
+    {if(expense.moment.valueOf()!==dateValue){
+        return expense;
     }
+    });
+    newTotal(allExpenses);
+    renderList(allExpenses);
+    
+}
 
-    renderList(newArr);
+//new total function to display new value of total on deletion of list items
+function newTotal(expenses){
+let sum=0;
+for(let i=0;i<expenses.length;i++){
+    sum=sum+allExpenses[i].amount;
+}
+totalExpense=sum;
+renderTotal(sum);
+}
+
+// render total in nav bar
+function renderTotal(totalExpense){
+    const someText=`Total : ${totalExpense}`;
+    headingEl.textContent=someText;
 
 }
+
 
 //view -- getting the updated array
 function renderList(arrOfList){
